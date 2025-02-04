e4799666508e04c16a0a284d433253bd716ac543
Pierre Dejoue <pierre.dejoue@gmail.com>
2025-02-04 13:55:29 +0100

    On macOS MainBoard::name() returns the HW model
    This is the same information as one will get from the command line:
    
        % sysctl hw.model
        hw.model: MacBookAir10,1

--- src/apple/mainboard.cpp
+++ src/apple/mainboard.cpp
@@ -5,14 +5,30 @@
 
 #ifdef HWINFO_APPLE
 
+#include <sys/sysctl.h>
+
+#include <sstream>
+#include <string>
+
 #include "hwinfo/mainboard.h"
 
 namespace hwinfo {
 
 // _____________________________________________________________________________________________________________________
 MainBoard::MainBoard() {
-  _vendor = "<unknown>";
-  _name = "<unknown>";
+  size_t size = 1024;
+  _vendor = "Apple";
+
+  std::string model_name;
+  model_name.resize(size);
+  if (sysctlbyname("hw.model", static_cast<void*>(const_cast<char*>(model_name.data())), &size, nullptr, 0) == 0) {
+    model_name.resize(size);  // trim the string to the actual size
+    model_name.pop_back();    // remove unprintable character at the end
+    _name = model_name;
+  } else {
+    _name = "<unknown>";
+  }
+
   _version = "<unknown>";
   _serialNumber = "<unknown>";
 }
